d1c8ef0ebf1ac793a3924850d1dbe6da5b7a28ff
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2022-07-08 06:59:28 +0300

    chore: upgrade deps in workflows (#23)
    * chore: upgrade deps in workflows

--- lib/core.dart
+++ lib/core.dart
@@ -3,4 +3,5 @@ export 'package:actions_toolkit_dart/src/core/core.dart';
 export 'package:actions_toolkit_dart/src/core/exit_code.dart';
 export 'package:actions_toolkit_dart/src/core/input_options.dart';
 export 'package:actions_toolkit_dart/src/core/models/summary_image_options.dart';
+export 'package:actions_toolkit_dart/src/core/models/summary_table_cell.dart';
 export 'package:actions_toolkit_dart/src/core/summary.dart';
